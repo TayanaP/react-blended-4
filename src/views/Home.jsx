eabb496505af4882114464ee6311e13b9edb09c5
Kirill <94064326+Fantom19@users.noreply.github.com>
2023-05-28 16:05:05 +0300

    add Hom

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,27 @@
-import { CocktailsList } from '../components/CocktailsList';
-import { Section } from '../components/Section';
-import { Loader } from '../components/Loader';
+import { CocktailsList } from "../components/CocktailsList";
+import { Section } from "../components/Section";
+import { Loader } from "../components/Loader";
+import { getTrendingCocktails } from "../api/cocktail-service";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [cocktails, setCoctails] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    getTrendingCocktails()
+      .then((data) => setCoctails(data))
+      .finally(() => setIsLoading(false));
+  }, []);
   return (
     <>
       <Section>
-        <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
+        <h1 className="text-center font-black text-gray-700 text-4xl mb-10">
           Trending cocktails
         </h1>
 
-        {/* <CocktailsList cocktails={cocktails} /> */}
+        <CocktailsList cocktails={cocktails} />
+        {isLoading && <Loader />}
       </Section>
     </>
   );
