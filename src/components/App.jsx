0e4d398a62b92c187096ec37bd9f8ad081e69005
Tetiana Pesotska <verhulev81@gmail.com>
2023-05-28 15:14:05 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { Layout } from '../layout/Layout/Layout';
-import { routes } from '../routes';
-import { CocktailDetail, Cocktails, Home } from '../views';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "../layout/Layout/Layout";
+import { routes } from "../routes";
+import { CocktailDetail, Cocktails, Home } from "../views";
 
 export const App = () => {
   return (
-    <h1 className='text-center font-black text-gray-700 text-4xl mb-10'>
-      Home Page
-    </h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="cocktails" elemen={<Cocktails />} />
+        <Route path="cocktails/:cocktailId" elemen={<CocktailDetail />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 };
